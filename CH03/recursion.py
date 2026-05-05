5245db6ee748b82d9b94b4fdd0a1abb72a8b8b2c
Huy Nguyen <huynguyen11232006@gmail.com>
2026-05-04 19:16:25 -0500

    Clean CH03 code

--- CH03/recursion.py
+++ CH03/recursion.py
@@ -17,10 +17,7 @@ def countdown(i: int) -> None:
     Base case: i <= 0
     Recursive case: print i, then countdown(i-1)
     """
-    # TODO: Implement countdown
-    # 1. If i <= 0, print 0 and return
-    # 2. Otherwise, print i
-    # 3. Call countdown(i - 1)
+
     if i <= 0:
         print(0)
         return
@@ -39,9 +36,7 @@ def fact(x: int) -> int:
     Base case: x == 1, return 1
     Recursive case: x * fact(x-1)
     """
-    # TODO: Implement factorial
-    # 1. If x <= 1, return 1 (base case)
-    # 2. Otherwise, return x * fact(x - 1)
+    
     if x <= 1:
         return 1
     else:
@@ -59,9 +54,7 @@ def recursive_sum(arr: List[int]) -> int:
         >>> recursive_sum([2, 4, 6])
         12
     """
-    # TODO: Implement recursive sum
-    # 1. If array is empty, return 0 (base case)
-    # 2. Otherwise, return arr[0] + recursive_sum(arr[1:])
+    
     if len(arr) == 0:
         return 0
     else:
@@ -75,9 +68,7 @@ def recursive_count(arr: List) -> int:
     Base case: empty array, return 0
     Recursive case: 1 + count of rest
     """
-    # TODO: Implement recursive count
-    # 1. If array is empty, return 0 (base case)
-    # 2. Otherwise, return 1 + recursive_count(arr[1:])
+    
     if len(arr) == 0:
         return 0
     else:
@@ -91,10 +82,7 @@ def recursive_max(arr: List[int]) -> int:
     Base case: single element, return it
     Recursive case: max of (first, max of rest)
     """
-    # TODO: Implement recursive max
-    # 1. If len(arr) == 1, return arr[0] (base case)
-    # 2. Get rest_max = recursive_max(arr[1:])
-    # 3. Return arr[0] if arr[0] > rest_max else rest_max
+    
     if len(arr) == 1:
         return arr[0]
     else:
